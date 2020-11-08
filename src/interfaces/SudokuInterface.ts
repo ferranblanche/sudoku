55d902ca95c7a71cbfbd379b93d30be87ec4763a
Ferran Blanché <ferran.blanche@gmail.com>
2020-11-08 13:00:47 +0100

    Merged Sudoku and Board models and interfaces

--- src/interfaces/SudokuInterface.ts
+++ src/interfaces/SudokuInterface.ts
@@ -1,6 +1,8 @@
 import { BoardType } from '../types/BoardType' 
 
-export interface BoardInterface {
+export interface SudokuInterface {
+    getBoard(): BoardType
     // writeMark(row: number, column: number, number: number): BoardType
+    validateNumber(row: number, column: number, number: number): boolean
     writeNumber(row: number, column: number, number: number): BoardType
 }
--- src/models/SudokuModel.ts
+++ src/models/SudokuModel.ts
@@ -1,16 +1,48 @@
-import { BoardModel } from './BoardModel'
+import { BoardType } from '../types/BoardType'
+import { SudokuInterface } from '../interfaces/SudokuInterface'
 
-interface ISudoku {
-    getBoard(): BoardModel
-}
-
-export class Sudoku implements ISudoku {
-    private board: BoardModel
+export class SudokuModel implements SudokuInterface {
+    private board: BoardType
     constructor(difficulty: "easy" | "medium" | "hard") {
-        this.board = new BoardModel()
+        this.board = [
+            [5, 3, , , 7, , , ,],
+            [6, , , 1, 9, 5, , ,],
+            [, 9, 8, , , , , 6,],
+            [8, , , , 6, , , , 3],
+            [4, , , 8, , 3, , , 1],
+            [7, , , , 2, , , , 6],
+            [, 6, , , , , 2, 8,],
+            [, , , 4, 1, 9, , , 5],
+            [, , , , 8, , , 7, 9]
+        ]
     }
-    public getBoard(): BoardModel {
+    public getBoard(): BoardType {
         return this.board
     }
-    
+    public writeNumber(rowIndex: number, columnIndex: number, number: number): BoardType {
+        if (!this.getCell(rowIndex, columnIndex) && this.validateNumber(rowIndex, columnIndex, number)) {
+            this.board[rowIndex][columnIndex] = number
+        }
+        return this.board
+    }
+    public validateNumber(rowIndex: number, columnIndex: number, number: number): boolean {
+        return this.validateArray(this.getRow(rowIndex), number) && this.validateArray(this.getColumn(columnIndex), number) && this.validateArray(this.getBlock(rowIndex, columnIndex), number)
+    }
+    private getCell(row: number, column: number): number {
+        return this.board[row][column]
+    }
+    private getRow(rowIndex: number): Array<number> {
+        return this.board[rowIndex]
+    }
+    private getColumn(columnIndex: number): Array<number> {
+        return this.board.map(row => row[columnIndex])
+    }
+    private getBlock(rowIndex: number, columnIndex: number): Array<number> {
+        let initialRow = Math.floor(rowIndex / 3) * 3
+        let initialColumn = Math.floor(columnIndex / 3) * 3
+        return this.board.slice(initialRow, initialRow + 3).map(row => row.slice(initialColumn, initialColumn + 3)).join().split(',').map(number => parseInt(number))
+    }
+    private validateArray(array: Array<number>, number: number): boolean {
+        return !array.includes(number)
+    }
 }
