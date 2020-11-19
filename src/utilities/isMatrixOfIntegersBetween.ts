d97451302292ac5006f63ca569312c35b666cfc2
Ferran Blanché <ferran.blanche@gmail.com>
2020-11-19 17:57:51 +0100

    Added tests for utility functions isMatrixSize() and isMatrixOfIntegersBetween()

--- src/utilities/isMatrixOfIntegersBetween.ts
+++ src/utilities/isMatrixOfIntegersBetween.ts
@@ -1,9 +1,9 @@
 import { isIntegerBetween } from "./isIntegerBetween";
 
-export function isMatrixOfIntegersBetween(matrix: number[][]): boolean {
+export function isMatrixOfIntegersBetween(matrix: number[][], min: number = 1, max: number = 9): boolean {
     for (let row of matrix) {
         for (let cell of row) {
-            if (!isIntegerBetween(cell)) { return false }
+            if (!isIntegerBetween(cell, min, max)) { return false }
         }
     }
     return true
